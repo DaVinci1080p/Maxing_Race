8c1a3dc5bee2c60c6991e79efc2b4d72a299ca17
RoyWendries <1747290wendries@zuyd.nl>
2022-05-18 00:03:27 +0200

    working search, skills pulled and dynamic skillcards

--- Maxing_Race/src/App.jsx
+++ Maxing_Race/src/App.jsx
@@ -1,36 +1,61 @@
 import { useEffect, useState } from 'react';
+import React from 'react';
 import './css/App.css';
 
-const profile_URL = 'https://apps.runescape.com/runemetrics/profile?user=';
-const App = () => {
-    const makeAPICall = async () => {
-        try {
-            const response = await fetch('https://localhost:3000/api/facts');
-            const data = await response.json();
-            console.log({ data });
-        } catch (e) {
-            console.log(e);
-        }
-    };
-    useEffect(() => {
-        makeAPICall();
-    }, []);
+import SkillCard from './components/SkillCard';
+import SearchIcon from './img/search.svg';
 
-    /*const searchPlayer = async (PlayerName) => {
-        const response = await fetch(`${profile_URL}${PlayerName}`);
+const profile_URL = 'https://apps.runescape.com/runemetrics/profile?user=';
+const cors_server = 'https://vincis-cors.herokuapp.com/';
+
+const App = () => {
+    const [player, setPlayer] = useState([]);
+    const [searchTerm, setSearchTerm] = useState('');
+
+    const searchPlayer = async (PlayerName) => {
+        const response = await fetch(`${cors_server}${profile_URL}${PlayerName}`);
         const data = await response.json();
 
-        console.log(data);
+        console.log(data.skillvalues);
+        setPlayer(data.skillvalues);
     };
     useEffect(() => {
-        searchPlayer('ReaperBoi');
+        searchPlayer('');
+    }, []);
+
+    const searchPlayer1 = async (PlayerName) => {
+        const response = await fetch(`${cors_server}${profile_URL}${PlayerName}`);
+        const data = await response.json();
+
+        console.log(data.skillvalues);
+        setPlayer1(data.skillvalues);
+    };
+    useEffect(() => {
+        searchPlayer1('');
     }, []);
 
     return (
-        <div className="App">
-            <h1>React Cors Guide</h1>
+        <div className="app">
+            <h1>Maxing competition</h1>
+            <div className="search">
+                <input
+                    placeholder="Search player"
+                    value={searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)}
+                />
+                <img src={SearchIcon} alt="search" onClick={() => searchPlayer(searchTerm)} />
+            </div>
+            {player?.length > 0 ? (
+                <div className="container">
+                    {React.Children.toArray(player.map((skill) => <SkillCard Skills={skill} />))}
+                </div>
+            ) : (
+                <div className="empty">
+                    <h2>No skills found</h2>
+                </div>
+            )}
         </div>
-    );*/
+    );
 };
 
 export default App;
---
+++ Maxing_Race/src/components/SkillCard.jsx
@@ -0,0 +1,55 @@
+const id_dict = {
+    0: 'Attack',
+    1: 'Defence',
+    2: 'Strength',
+    3: 'Constitution',
+    4: 'Ranged',
+    5: 'Prayer',
+    6: 'Magic',
+    7: 'Cooking',
+    8: 'Woodcutting',
+    9: 'Fletching',
+    10: 'Fishing',
+    11: 'Firemaking',
+    12: 'Crafting',
+    13: 'Smithing',
+    14: 'Mining',
+    15: 'Herblore',
+    16: 'Agility',
+    17: 'Thieving',
+    18: 'Slayer',
+    19: 'Farming',
+    20: 'Runecrafting',
+    21: 'Hunter',
+    22: 'Construction',
+    23: 'Summoning',
+    24: 'Dungeoneering',
+    25: 'Divination',
+    26: 'Invention',
+    27: 'Archaeology'
+};
+
+const SkillCard = ({ Skills }) => {
+    return (
+        <div className="skill">
+            <div>
+                <p>Rank: {Skills.rank.toLocaleString('en-US')}</p>
+            </div>
+            <div>
+                <img
+                    src={
+                        `./img/${id_dict[Skills.id]}` == 'NULL'
+                            ? `./img/${id_dict[Skills.id]}`
+                            : 'https://via.placeholder.com/400'
+                    }
+                    alt={id_dict[Skills.id]}></img>
+            </div>
+            <div>
+                <span>{id_dict[Skills.id]}</span>
+                <h3>{Skills.level}</h3>
+            </div>
+        </div>
+    );
+};
+
+export default SkillCard;
