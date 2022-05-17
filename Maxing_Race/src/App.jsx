138552c9cf04cd63bca4238402d95352303a345e
RoyWendries <1747290wendries@zuyd.nl>
2022-05-17 17:33:06 +0200

    pre package-lock deletion

--- Maxing_Race/src/App.jsx
+++ Maxing_Race/src/App.jsx
@@ -1,9 +1,36 @@
-import Welcome from './components/Welcome';
+import { useEffect, useState } from 'react';
+import './css/App.css';
 
-export default function App() {
+const profile_URL = 'https://apps.runescape.com/runemetrics/profile?user=';
+const App = () => {
+    /*const makeAPICall = async () => {
+        try {
+            const response = await fetch('http://localhost:8080/cors', { mode: 'cors' });
+            const data = await response.json();
+            console.log({ data });
+        } catch (e) {
+            console.log(e);
+        }
+    };
+    useEffect(() => {
+        makeAPICall();
+    }, []);*/
+
+    const searchPlayer = async (PlayerName) => {
+        const response = await fetch(`${profile_URL}${PlayerName}`);
+        const data = await response.json();
+
+        console.log(data);
+    };
+    useEffect(() => {
+        searchPlayer('ReaperBoi');
+    }, []);
+
     return (
-        <>
-            <Welcome />
-        </>
+        <div className="App">
+            <h1>React Cors Guide</h1>
+        </div>
     );
-}
+};
+
+export default App;
