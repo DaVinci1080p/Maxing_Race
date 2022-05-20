3f5203b06088f048b8734cb3cb67777cffe2593c
RoyWendries <1747290wendries@zuyd.nl>
2022-05-20 12:53:51 +0200

    Added search enter function, flavicon

--- Maxing_Race/src/App.jsx
+++ Maxing_Race/src/App.jsx
@@ -9,53 +9,52 @@ const profile_URL = 'https://apps.runescape.com/runemetrics/profile?user=';
 const cors_server = 'https://vincis-cors.herokuapp.com/';
 
 const App = () => {
-    const [player, setPlayer] = useState([]);
-    const [searchTerm, setSearchTerm] = useState('');
+  const [player, setPlayer] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
 
-    const searchPlayer = async (PlayerName) => {
-        const response = await fetch(`${cors_server}${profile_URL}${PlayerName}`);
-        const data = await response.json();
+  const searchPlayer = async (PlayerName) => {
+    const response = await fetch(`${cors_server}${profile_URL}${PlayerName}`);
+    const data = await response.json();
 
-        console.log(data.skillvalues);
-        setPlayer(data.skillvalues);
-    };
-    useEffect(() => {
-        searchPlayer('');
-    }, []);
+    console.log(data.skillvalues);
+    setPlayer(data.skillvalues);
+  };
+  useEffect(() => {
+    searchPlayer('');
+  }, []);
 
-    const searchPlayer1 = async (PlayerName) => {
-        const response = await fetch(`${cors_server}${profile_URL}${PlayerName}`);
-        const data = await response.json();
-
-        console.log(data.skillvalues);
-        setPlayer1(data.skillvalues);
-    };
-    useEffect(() => {
-        searchPlayer1('');
-    }, []);
-
-    return (
-        <div className="app">
-            <h1>Maxing competition</h1>
-            <div className="search">
-                <input
-                    placeholder="Search player"
-                    value={searchTerm}
-                    onChange={(e) => setSearchTerm(e.target.value)}
-                />
-                <img src={SearchIcon} alt="search" onClick={() => searchPlayer(searchTerm)} />
-            </div>
-            {player?.length > 0 ? (
-                <div className="container">
-                    {React.Children.toArray(player.map((skill) => <SkillCard Skills={skill} />))}
-                </div>
-            ) : (
-                <div className="empty">
-                    <h2>No skills found</h2>
-                </div>
-            )}
+  return (
+    <div className="app">
+      <h1>Maxing competition</h1>
+      <div className="search">
+        <input
+          placeholder="Search player"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          onKeyPress={(e) => {
+            if (e.key === 'Enter') {
+              searchPlayer(searchTerm);
+            }
+          }}
+        />
+        <img
+          src={SearchIcon}
+          alt="search"
+          onClick={() => searchPlayer(searchTerm)}
+        />
+      </div>
+      {player?.length > 0 ? (
+        <div className="container">
+          {React.Children.toArray(
+            player.map((skill) => <SkillCard Skills={skill} />)
+          )}
         </div>
-    );
+      ) : (
+        <div className="empty">
+          <h2>No skills found</h2>
+        </div>
+      )}
+    </div>
+  );
 };
-
 export default App;
